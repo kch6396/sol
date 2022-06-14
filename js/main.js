e8095b233ba99904ceff31d8fe13447399b15ae0
kch6396 <cjfghks6396@gmail.com>
2022-06-14 14:50:11 +0900

    5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 
-const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function (fadeEl, index){
-  gsap.to(fadeEl, 1, {
-    delay: (index+1) * .7,
-    opacity: 1
-  });
+// const fadeEls = document.querySelectorAll('.visual .fade-in');
+// fadeEls.forEach(function (fadeEl, index){
+//   gsap.to(fadeEl, 1, {
+//     delay: (index+1) * .7,
+//     opacity: 1
+//   });
+// });
+
+const toggleBtn=document.querySelector('.nav__button');
+const menu= document.querySelector('.nav');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
 });
