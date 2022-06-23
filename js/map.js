85d4e24bfeb621b642cc2b025d8675db0aa341ec
kch6396 <cjfghks6396@gmail.com>
2022-06-23 19:12:57 +0900

    55

--- js/map.js
+++ js/map.js
@@ -1,12 +1,70 @@
-var map = new naver.maps.Map('map', {
-  center: new naver.maps.LatLng(37.3595704, 127.105399), //지도의 초기 중심 좌표
-  zoom: 1, //지도의 초기 줌 레벨
-  minZoom: 7, //지도의 최소 줌 레벨
-  zoomControl: true, //줌 컨트롤의 표시 여부
-  zoomControlOptions: { //줌 컨트롤의 옵션
-      position: naver.maps.Position.TOP_RIGHT
-  }
+// var container = document.getElementById('map');
+//         var options = {
+//             center: new kakao.maps.LatLng( 35.563442697386925,  129.32253853048218 ),
+//         level: 3
+//         };
+
+//         var map = new kakao.maps.Map(container, options);
+
+//         var markerPosition  = new kakao.maps.LatLng(35.563442697386925,  129.32253853048218 );
+
+// // 마커를 생성합니다
+// var marker = new kakao.maps.Marker({
+//     position: markerPosition
+// });
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(35.563442697386925,  129.32253853048218), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+        scrollwheel:false
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+var markerPosition  = new kakao.maps.LatLng(35.563442697386925,  129.32253853048218 );
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPLEFT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.LEFT);
+
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
 });
 
 
-var map = new GMap2(document.getElementById("map"));map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+
+
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+
+var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    content : iwContent,
+    removable : iwRemoveable
+});
+
+
+var iwContent = '<div style="padding:5px;">Hello World! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    position : iwPosition, 
+    content : iwContent 
+});
+
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+infowindow.open(map, marker); 
