4712a2650b6486de76e222cd4a8751eb947a9a2b
kch6396 <cjfghks6396@gmail.com>
2022-06-29 17:21:09 +0900

    83

--- js/map.js
+++ js/map.js
@@ -70,6 +70,25 @@ kakao.maps.event.addListener(marker, 'click', function() {
 });
 
 
+// var markerPosition = marker.getPosition(); 
+// map.relayout();
+// map.setCenter(markerPosition);
+function resizeMap() {
+    var mapContainer = document.getElementById('map');
+    mapContainer.style.width = '650px';
+    mapContainer.style.height = '650px'; 
+}
+
+function relayout() {    
+    
+    // 지도를 표시하는 div 크기를 변경한 이후 지도가 정상적으로 표출되지 않을 수도 있습니다
+    // 크기를 변경한 이후에는 반드시  map.relayout 함수를 호출해야 합니다 
+    // window의 resize 이벤트에 의한 크기변경은 map.relayout 함수가 자동으로 호출됩니다
+    map.relayout();
+}
+
+
+
 const spyEls = document.querySelectorAll('div.scroll-spy')
 // 요소들 반복 처리!
 spyEls.forEach(function (spyEl) {
